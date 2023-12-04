2a34c157b00c427ea42d9d5b1156a336dfb921e4
Marzin <70151472+Marzin-bot@users.noreply.github.com>
2023-12-04 13:53:46 +0100

    Update module.js
    add a loop that instantiates multiple Vector3.

--- Bug FFI/module.js
+++ Bug FFI/module.js
@@ -2,9 +2,11 @@ import KuwazyEngine from "./kuwazy_engine/wrapper/javascript/kuwazy_engine.mjs";
 
 export default class GameServer{
     constructor (){
-        const vecA = new KuwazyEngine.Vector3(1, 2, 3); // That works!
-        console.log(Date.now(), vecA.x, vecA.y, vecA.z);
-
+        for (let i = 0; i < 1000; i++){
+            const vecA = new KuwazyEngine.Vector3(1, 2, 3);
+            console.log(i, vecA.x, vecA.y, vecA.z);
+        }
+        
         setInterval(() => {
             const vecB = new KuwazyEngine.Vector3(1, 2, 3); // Bug ? (exited with code 11 (SIGSEGV))
             console.log(Date.now(), vecB.x, vecB.y, vecB.z);
